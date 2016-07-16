7bafcb05fd43edc1f8a4ad0e8b19b7bd2a3614df
Jeremy Magland <jeremy.magland@gmail.com>
2016-07-16 11:38:17 -0400

    dev

--- mountainviewweb/controlwidgets/generalcontrolwidget.js
+++ mountainviewweb/controlwidgets/generalcontrolwidget.js
@@ -4,8 +4,23 @@ function GeneralControlWidget(O,mvcontext,main_window) {
 	O.div().addClass('GeneralControlWidget');
 	O.setSize(0,200);
 
-	var B=P.createButtonControl('test_button','test button',function() {
-		mvcontext.setOption('clip_size',30);
-	});
-	O.div().append(B);
+	var table0=$('<table><tr>Clip size:</tr></table>');
+	O.div().append(table0);
+	{
+		var X=P.createIntControl('clip_size');
+		var tr=$('<tr></tr>'); tr.append(X);
+		O.div().append(tr);
+	}
+	{
+		var X=P.createButtonControl('apply','Apply',function() {
+			mvcontext.setOption('clip_size',P.controlValue('clip_size'));
+		});
+		var tr=$('<tr></tr>'); tr.append(X);
+		O.div().append(tr);
+	}
+	JSQ.connect(mvcontext,'optionsChanged',O,update_controls);
+	function update_controls() {
+		P.setControlValue('clip_size',mvcontext.option('clip_size'));
+	}
+	update_controls();
 }
--- mountainviewweb/core/mvabstractcontrolwidget.js
+++ mountainviewweb/core/mvabstractcontrolwidget.js
@@ -20,27 +20,36 @@ function MVAbstractControlWidget(O,mvcontext,main_window) {
 	P.createButtonControl=function(name,label,callback) {return createButtonControl(name,label,callback);};
 
 	function controlValue(name) {
+		if  (name in m_int_controls) return Number(m_int_controls[name].val());
+		if  (name in m_double_controls) return Number(m_double_controls[name].val());
 		return 0;
 	}
 	function setControlValue(name,val) {
-
+		if (name in m_int_controls) m_int_controls[name].val(val);
+		if (name in m_double_controls) m_double_controls[name].val(val);
 	}
 	function setControlEnabled(name,val) {
-
+		//TODO
 	}
 	function createIntControl(name) {
 		var ret=$('<input type=text></input>');
+		m_int_controls[name]=ret;
 		return ret;
 	}
 	function createDoubleControl(name) {
 		var ret=$('<input type=text></input>');
+		m_double_controls[name]=ret;
 		return ret;
 	}
 	function createButtonControl(name,label,callback) {
 		var ret=$('<button>'+label+'</button>');
 		ret.click(callback);
+		m_button_controls[name]=ret;
 		return ret;
 	}
 
+	var m_int_controls=[];
+	var m_double_controls=[];
+	var m_button_controls=[];
 	return P;
 }
