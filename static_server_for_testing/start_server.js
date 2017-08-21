7517bc6995e08b6124b0b60a82108fad70ff3c5e
Jeremy Magland <jeremy.magland@gmail.com>
2017-08-21 06:49:02 -0400

    static server

---
+++ static_server_for_testing/start_server.js
@@ -0,0 +1,12 @@
+// Instructions:
+// npm install connect serve-static
+// nodejs start_server.js [port_number]
+var connect = require('connect');
+var args=process.argv.slice(2);
+var listen_port=args[0]||8040;
+var directory=args[1]||__dirname;
+var serveStatic = require('serve-static');
+connect().use(serveStatic(directory)).listen(listen_port, function(){
+	console.log('Serving directory: '+directory);
+    console.log('Server running on port '+listen_port+'...');
+});
